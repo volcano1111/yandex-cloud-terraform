752d2982389eae76d498c1ad5c1d21bcce5ebb3e
volcano1111 <acckaya.sotona@gmail.com>
2020-08-04 16:08:35 +0300

    Fixed errors in VM config. Fixed wrong token.

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "yandex" {
-  token     = "9d3d05d67ab94f138c1c52ff4226e55b"
+  token     = "AgAAAABEF8dOAATuwUSgFnNQfElwrtH3_8lgmcs"
   cloud_id  = "b1ghh560851m530rua1u"
   folder_id = "b1gg8lei5uc8ba0ilm8h"
   zone      = "ru-central1-a"
@@ -9,7 +9,7 @@ resource "yandex_compute_instance" "vm-1" {
   name = "mongodb1"
 
   resources {
-    cores  = 1
+    cores  = 2
     memory = 2
   }
 
@@ -33,7 +33,7 @@ resource "yandex_compute_instance" "vm-2" {
   name = "mongodb2"
 
   resources {
-    cores  = 1
+    cores  = 2
     memory = 2
   }
 
@@ -57,7 +57,7 @@ resource "yandex_compute_instance" "vm-3" {
   name = "mongodb3"
 
   resources {
-    cores  = 1
+    cores  = 2
     memory = 2
   }
 
